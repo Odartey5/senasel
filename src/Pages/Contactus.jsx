048570530f9d3334cb00fd352b468fd6aa0a672a
nii fakye <raymondank22@gmail.com>
2025-05-19 15:12:47 +0000

    minor changes

--- src/Pages/Contactus.jsx
+++ src/Pages/Contactus.jsx
@@ -2,25 +2,25 @@ import './Contactus.css';
 
 export default function Contact() {
   return (
-    <div>
-      <h2>Contact Us</h2>
-      <form>
-        <label>
+    <div className='contact-container'>
+      <h2 className='contact-header'>Contact Us</h2>
+      <form className='contact-form'>
+        <label className='contact-label'>
           Name:
           <input type="text" name="name" required />
         </label>
         <br />
-        <label>
+        <label className='contact-label'>
           Email:
           <input type="email" name="email" required />
         </label>
         <br />
-        <label>
+        <label className='contact-label'>
           Message:
           <textarea name="message" required></textarea>
         </label>
         <br />
-        <button type="submit">Send</button>
+        <button className="contact-button" type="submit">Send</button>
       </form>
     </div>
   );
